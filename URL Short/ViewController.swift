60c86bc0984af310bd93e3f5b1c17c82f970e6dc
Jack Doherty <jackdoherty@Jacks-MacBook-Pro.local>
2017-06-11 23:29:14 -0400

    made new VCs for registration and dashboard, got labels and text fields set up for login, added initial login state listener

--- URL Short/ViewController.swift
+++ URL Short/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    
+    
+    var handle: AuthStateDidChangeListenerHandle?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            //code for when auth state changes
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
+    
+    //IBActions
+    
+    @IBAction func UILoginHit(_ sender: Any) {
+        Auth.auth().signIn(withEmail: email.text!, password: pass.text!) { (user, error) in
+            //code
+        }
+    }
+    @IBAction func UIRegisterHIt(_ sender: Any) {
+        //code
+    }
+    
 }
 
+//finished on signing in existing users, making view controllers, and starting registration
